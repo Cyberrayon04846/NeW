9a7ddc9606ab9f662d1b4339a5e2c6fc4899ed6f
Rayon <cyberrayon@gmail.com>
2022-01-21 09:05:59 +0530

    main

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-console.log("this is  script.js");
+// Declaring the main variables
 let newscontainer = document.getElementById("newscontainer");
 const api = "3537479e19e84c528b25dbb34acd99c9";
+let html = "";
+// async function to fetch news  with the help of api
 async function fetchnews() {
-  let response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=in&apiKey=${api}`
-  );
+  let response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${api}` );
   let user = await response.json();
   return user;
 }
-let html = '';
+
 let news = fetchnews();
+// function to show news 
 news.then((data) => {
-    console.log(data.articles);
-   let newsaccodin = data.articles
+  console.log(data.articles);
+  let newsaccodin = data.articles;
   newsaccodin.forEach((element) => {
     console.log(html.length);
 
-    
-    let article =`<div class="box news"  >
+    let article = `<div class="box news"  >
     <div class="col-sm-6">
       <div class="card">
         <div class="card-body">
@@ -26,12 +26,10 @@ news.then((data) => {
           <a href="${element.url}" class="btn btn-primary">Read More</a>
         </div>
       </div>
-    </div>`
-  
- 
-  html += article;
-   
-  }); 
- 
-  newscontainer.innerHTML = html ;
+    </div>`;
+
+    html += article;
+  });
+
+  newscontainer.innerHTML = html;
 });
